93b92a48c0e7ce0b28954eb6d644e9982f993ac1
Arcaniax <lesley.dom@hotmail.com>
2020-12-21 16:56:06 +0100

    Update HeadDatabaseAPI.java

--- src/main/java/me/arcaniax/hdb/api/HeadDatabaseAPI.java
+++ src/main/java/me/arcaniax/hdb/api/HeadDatabaseAPI.java
@@ -30,6 +30,10 @@ import me.arcaniax.hdb.object.head.Head;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;	
+import java.util.List;	
+import java.util.UUID;
+
 public class HeadDatabaseAPI {
 
     /**

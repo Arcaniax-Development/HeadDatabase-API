50e474f75e20457feccbfcbe46df1aac61014b49
Alexander Brandes <mc.cache@web.de>
2025-04-19 20:58:21 +0200

    Adapt new central portal
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import com.diffplug.gradle.spotless.SpotlessPlugin
 import java.net.URI
 
 plugins {
     java
     `java-library`
-    `maven-publish`
     signing
 
-    id("com.diffplug.spotless") version "6.25.0"
+    id("com.diffplug.spotless") version "7.0.3"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.31.0"
 
     idea
     eclipse
@@ -28,7 +29,7 @@ version = "1.3.3-SNAPSHOT"
 repositories {
     maven {
         name = "PaperMC"
-        url = uri("https://papermc.io/repo/repository/maven-public/")
+        url = uri("https://repo.papermc.io/repository/maven-public/")
     }
 }
 
@@ -45,24 +46,9 @@ spotless {
 
 tasks {
 
-    compileJava {
-        options.compilerArgs.addAll(arrayOf("-Xmaxerrs", "1000"))
-        options.compilerArgs.add("-Xlint:all")
-        for (disabledLint in arrayOf("processing", "path", "fallthrough", "serial"))
-            options.compilerArgs.add("-Xlint:$disabledLint")
-        options.isDeprecation = true
-        options.encoding = "UTF-8"
-    }
-
     javadoc {
         val opt = options as StandardJavadocDocletOptions
-        opt.addStringOption("Xdoclint:none", "-quiet")
-        opt.tags(
-            "apiNote:a:API Note:",
-            "implSpec:a:Implementation Requirements:",
-            "implNote:a:Implementation Note:"
-        )
-        opt.links("https://jd.papermc.io/paper/1.20/")
+        opt.links("https://jd.papermc.io/paper/1.21.5/")
         opt.noTimestamp()
     }
 
@@ -72,10 +58,6 @@ tasks {
     }
 }
 
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
 
 signing {
     if (!project.hasProperty("skip.signing") && !version.toString().endsWith("-SNAPSHOT")) {
@@ -87,53 +69,50 @@ signing {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+mavenPublishing {
+    coordinates(
+            groupId = "$group",
+            artifactId = project.name,
+            version = "${project.version}",
+    )
 
-            pom {
+    pom {
+        name.set(project.name)
+        description.set("The API for Head Database, a Minecraft plugin that allows you to obtain thousands of custom Minecraft skulls that feature unique designs.")
+        url.set("https://github.com/Arcaniax-Development/HeadDatabase-API")
 
-                name.set(project.name + " " + project.version)
-                description.set("The API for Head Database, a Minecraft plugin that allows you to obtain thousands of custom Minecraft skulls that feature unique designs.")
-                url.set("https://github.com/Arcaniax-Development/HeadDatabase-API")
-
-                licenses {
-                    license {
-                        name.set("GNU General Public License, Version 3.0")
-                        url.set("https://www.gnu.org/licenses/gpl-3.0.html")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Arcaniax")
-                        name.set("Arcaniax")
-                    }
-                }
-
-                scm {
-                    url.set("https://github.com/Arcaniax-Development/HeadDatabase-API")
-                    connection.set("scm:git:https://github.com/Arcaniax-Development/HeadDatabase-API.git")
-                    developerConnection.set("scm:git:git@github.com:Arcaniax-Development/HeadDatabase-API.git")
-                    tag.set("${project.version}")
-                }
-
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("https://github.com/Arcaniax-Development/HeadDatabase-API/issues")
-                }
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/mit")
+                distribution.set("repo")
             }
         }
-    }
-}
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        developers {
+            developer {
+                id.set("Arcaniax")
+                name.set("Arcaniax")
+            }
+            developer {
+                id.set("NotMyFault")
+                name.set("NotMyFault")
+                email.set("contact(at)notmyfault.dev")
+            }
         }
+
+        scm {
+            url.set("https://github.com/Arcaniax-Development/HeadDatabase-API")
+            connection.set("scm:git:https://github.com/Arcaniax-Development/HeadDatabase-API.git")
+            developerConnection.set("scm:git:git@github.com:Arcaniax-Development/HeadDatabase-API.git")
+            tag.set(project.version.toString())
+        }
+
+        issueManagement {
+            system.set("GitHub")
+            url.set("https://github.com/Arcaniax-Development/HeadDatabase-API/issues")
+        }
+
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     }
 }
